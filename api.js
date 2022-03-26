3d570ad2d58827ef5b0705a93a2b896ca58a588d
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-03-26 16:58:26 -0300

    Html and css prototype done.

--- api.js
+++ api.js
@@ -1,15 +1,13 @@
 const key = '046d5f77d2222f5d755e958810af5830'
-const city = 'Boston'
 
-async function CreateApi(){
+async function CreateApi(city, key){
     const weather_api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`)
     try{
         const response = await weather_api.json()
-        console.log('---Working---')
         console.log(response)
     }catch{
         console.error(error)
     }
 }
 
-CreateApi()
+CreateApi('Boston', key)
