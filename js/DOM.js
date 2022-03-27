fb0260eb5a1c65284d1a0caed8b71f4b812c6c4f
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-03-27 11:46:33 -0300

    DOM script render base done.

--- js/DOM.js
+++ js/DOM.js
@@ -1,6 +1,8 @@
 import { CreateApi } from "./api.js";
 
-CreateApi('Boston');
+const api_list = await CreateApi('Boston');
+
+console.log(api_list)
 
 const WeatherList = document.getElementById('weather_list');
 
@@ -28,11 +30,24 @@ function WeatherMainBox(){
     //box-sub-info
     const box_sub_info = DOMcreator('div', 'box-sub-info');
     const feels_like = DOMcreator('h6');
-    const pressure = DOMcreator('h6');
+
+    const minMax_temp = DOMcreator('div', 'minMax-div');
+    const min_temp = DOMcreator('h6', 'max-temp');
+    const max_temp = DOMcreator('h6', 'min-temp');
     const humidity = DOMcreator('h6');
 
+    weather_state.textContent = api_list.weather[0].description;
+    weather_icon.src = `http://openweathermap.org/img/w/${api_list.weather[0].icon}.png`;
+    weather_local.textContent = `${api_list.name}, ${api_list.sys.country}`;
+    main_temp.textContent = Math.round(api_list.main.temp);
+    min_temp.textContent = `Min: ${Math.round(api_list.main.temp_min)}`;
+    max_temp.textContent = `Max: ${Math.round(api_list.main.temp_max)}`;
+    minMax_temp.append(min_temp, max_temp);
+    feels_like.textContent = `Feels like: ${api_list.main.feels_like}`;
+    humidity.textContent = `Humidty: ${api_list.main.humidity}`;
+
     main_temp_div.appendChild(main_temp);
-    box_sub_info.append(feels_like, pressure, humidity);
+    box_sub_info.append(feels_like, minMax_temp, humidity);
     box_info.append(main_temp_div, box_sub_info);
     box_head.append(weather_state, weather_icon);
     box_content.append(weather_local, hr_line, box_info);
--- js/api.js
+++ js/api.js
@@ -3,8 +3,7 @@ const key = '046d5f77d2222f5d755e958810af5830';
 async function CreateApi(city){
     const weather_api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`);
     try{
-        const response = await weather_api.json();
-        console.log(response);
+        return weather_api.json();
     }catch{
         console.error(error);
     }
