bf4ac47b70acf07593aa2c07950a3462ec70511f
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-03-31 10:28:14 -0300

    Fixed minor things.

--- js/api.js
+++ js/api.js
@@ -12,6 +12,7 @@ async function WeatherApi(city){
 async function ForecastApi(city){
     try{
         const forecast_api = (await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${key}`)).json();
+        console.log(await forecast_api)
         return await forecast_api;
     }catch{
         console.error(error);
